d6ea06a7c12ae3a1eafa967589a8fd2c4ef3fc81
Tanzil Siddique <180063675@aston.ac.uk>
2024-11-28 18:43:40 +0000

    Account and Admin
    Implemented account/Admin login functionality - moved all HTML/CSS?IMG files into my_first_app/public

--- my_first_app/database/migrations/orders.php
+++
@@ -1,8 +0,0 @@
-<?php
-Schema::create('orders', function (Blueprint $table) {
-    $table->id();
-    $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Reference to users
-    $table->decimal('total', 10, 2);
-    $table->enum('status', ['pending', 'processing', 'shipped', 'delivered', 'returned', 'cancelled'])->default('pending');
-    $table->timestamps();
-});
