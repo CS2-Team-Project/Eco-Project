91905aba5e33a2711368f5baacb8bb67498970df
Isaac Armas <230156328@aston.ac.uk>
2024-12-06 16:04:27 +0000

    Revert "Reapply "Merge branch 'stylecss' into p-b-c-test""
    This reverts commit 794d736af41c5fc9fd7ec13a33c0b9e797bf4be4.

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
-// Dark mode functionality
-let darkmode = localStorage.getItem('darkmode');
-const themeSwitch = document.getElementById('theme-switch');
 
-const enableDarkmode = () => {
-    document.body.classList.add('darkmode');
-    localStorage.setItem('darkmode', 'active');
-}
-
-const disableDarkmode = () => {
-    document.body.classList.remove('darkmode');
-    localStorage.setItem('darkmode', null);
-}
-
-if (darkmode === "active") enableDarkmode();
-
-// Event listener for theme toggle button
-themeSwitch.addEventListener("click", (event) => {
-    event.preventDefault(); // Prevent the default button behavior
-    darkmode = localStorage.getItem('darkmode');
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
-});
-
-// Size button functionality
+//size button selected
 document.querySelectorAll('.size-buttons').forEach(buttonGroup => {
     buttonGroup.addEventListener('click', event => {
         if (event.target.classList.contains('size')) {
@@ -35,10 +13,9 @@ document.querySelectorAll('.size-buttons').forEach(buttonGroup => {
     });
 });
 
-// Open the products page when the "Shop Now" button is clicked
-document.querySelector("#herobanner button").addEventListener("click", function () {
-    // Open 'products.html' page in a new tab
+
+// Use querySelector to target the first button on the page
+document.querySelector("button").addEventListener("click", function () {
+    // When the button is clicked, open the 'products.html' page in a new tab
     window.open('products.html', '_blank');
-    
 });
- 
