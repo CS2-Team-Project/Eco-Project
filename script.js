9f0586c0cf2c592a232e403c174c03f99cc87c10
Adinventias <adinventias@gmail.com>
2024-12-06 00:30:17 +0000

    updated theme with button control, also created variables with each colour and synergized it with the darktheme colours, also made readability clearer and altered image background to sync with the theme changes

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+// Dark mode functionality
+let darkmode = localStorage.getItem('darkmode');
+const themeSwitch = document.getElementById('theme-switch');
 
-//size button selected
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+}
+
+if (darkmode === "active") enableDarkmode();
+
+// Event listener for theme toggle button
+themeSwitch.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent the default button behavior
+    darkmode = localStorage.getItem('darkmode');
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
+
+// Size button functionality
 document.querySelectorAll('.size-buttons').forEach(buttonGroup => {
     buttonGroup.addEventListener('click', event => {
         if (event.target.classList.contains('size')) {
@@ -13,9 +35,8 @@ document.querySelectorAll('.size-buttons').forEach(buttonGroup => {
     });
 });
 
-
-// Use querySelector to target the first button on the page
-document.querySelector("button").addEventListener("click", function () {
-    // When the button is clicked, open the 'products.html' page in a new tab
+// Open the products page when the "Shop Now" button is clicked
+document.querySelector("#herobanner button").addEventListener("click", function () {
+    // Open 'products.html' page in a new tab
     window.open('products.html', '_blank');
 });
